0e41895a183c98689586db27d2b2671b6bc0c735
aversan <aversan@yandex.ru>
2016-08-17 14:57:55 +0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+require('../blocks/mdlComponentHandler/mdlComponentHandler.js');
+require('../blocks/mdl-icon-toggle/mdl-icon-toggle');
+require('../blocks/uk-core/uk-core.js');
+require('../blocks/uk-sticky/uk-sticky.js');
+require('../blocks/util/util.js');
+require('../blocks/collapse/collapse.js');
+require('../blocks/treeview/treeview.js');
+require('../blocks/bootstrap-filestyle/bootstrap-filestyle.js');
+
+$(() => {
+    $('.panel .collapse-toggle').on('click', function() {
+        var target = $(this).data('target');
+        var $primaryTarget = $(this).parents('.panel').find(target);
+
+        if ($primaryTarget.hasClass('in')) {
+            $(target).collapse('hide');
+        } else {
+            $(target).collapse('show');
+        }
+    });
+});
